c16bf4ae82afa15444ef3c3d7f79b37645628253
Ana Joyce <anajoyce2066@outlook.com>
2023-10-17 23:36:17 -0300

    updates

---
+++ src/edu/ana/segundaaula/Operadores.java
@@ -0,0 +1,111 @@
+package edu.ana.segundaaula;
+
+public class Operadores {
+    public static  void main(String[] args) {
+
+         /*
+
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+
+       // concatenaçâo
+
+        String concatenacao ="?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.print(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.print(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.print(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.print(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.print(concatenacao);
+
+
+
+      // positivo ou negativo
+
+        int numero = 5;
+
+        System.out.println( - numero);
+        System.out.println(numero);
+
+
+
+        // repeticao e incrementação
+
+
+        int numero = 5;
+
+        //incrementando numero em mais 1 numero, imprime 6
+
+        System.out.println( ++ numero);
+        System.out.println(numero);
+
+        //decrementa numero em menos 1 numero, imprime 5
+
+        System.out.println( -- numero);
+        System.out.println(numero);
+
+
+        //ordem de precedencia conta aqui
+
+        boolean variavel = true;
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
+
+
+
+        int a,b;
+        a=6;
+        b=6;
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+
+
+        int n1 = 1;
+        int n2 = 3;
+
+        boolean simNao = n1 == n2; //false
+        System.out.println("n1 é IGUAL a n2?" + simNao);
+
+        simNao = n1 != n2; //true
+        System.out.println("n1 é DIFERENTE a n2?" + simNao);
+
+        simNao = n1 > n2; //false
+        System.out.println("n1 é MAIOR a n2?" + simNao);
+
+        simNao = n1 < n2; //true
+        System.out.println("n1 é MENOR a n2?" + simNao);
+
+*/
+        //Logicos
+
+        boolean c1 = true;
+        boolean c2 = false;
+
+        if(c1 && c2){
+            System.out.println("as duas sao verdadeiras");
+        }
+        if(c1 || c2){
+            System.out.println("uma das condições é verdadeira");
+        }
+    }
+
+}
+
